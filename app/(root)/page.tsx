0f8b84ac7c109adcf324b6a6aa5d7855769dadbe
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2023-12-15 22:32:57 -0300

    feat: Hero section added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,45 @@
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main>
+    <>
+      <section className='bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10'>
+        <div className='wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0'>
 
-    </main>
+
+          <div className='flex flex-col justify-center gap-8'>
+            <h1 className='h1-bold'>O seu evento começa aqui! No MegaHub.</h1>
+            <p className='p-regular-20 md:p-regular-24'>
+              Participe de eventos, crie o seu evento, conecte-se a todos, tudo em um só lugar!
+            </p>
+            <Button size="lg" asChild className='button w-full sm:w-fit'>
+              <Link href="#events">
+                Explorar eventos
+              </Link>
+            </Button>
+          </div>
+
+          <Image
+            src="/assets/images/hero.png"
+            alt="hero"
+            width={1000}
+            height={1000}
+            className='max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]'
+          />
+
+        </div>
+      </section>
+
+      <section id='events' className='wrapper my-8 flex flex-col gap-8 md:gap-12'>
+        <h2 className='h2-bold'>Centenas de eventos <br /> a um clique de distância.</h2>
+
+        <div className='flex w-full flex-col gap-5 md:flex-row'>
+          Pesquisar 
+          FiltroDeCategorias
+        </div>
+      </section>
+    </>
   )
 }
