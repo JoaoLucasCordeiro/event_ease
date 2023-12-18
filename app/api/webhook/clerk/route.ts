217e83f5fadb45d815ce654aadee7bf711ef2598
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2023-12-18 19:46:01 -0300

    fix: fix imports in the route clerk

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,7 +1,6 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
-
 import { clerkClient } from '@clerk/nextjs'
 import { NextResponse } from 'next/server'
 import { createUser, deleteUser, updateUser } from '@/lib/actions/user.action'
@@ -21,23 +20,23 @@ export async function POST(req: Request) {
   const svix_timestamp = headerPayload.get("svix-timestamp");
   const svix_signature = headerPayload.get("svix-signature");
  
- 
+  // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
     return new Response('Error occured -- no svix headers', {
       status: 400
     })
   }
  
- 
+  // Get the body
   const payload = await req.json()
   const body = JSON.stringify(payload);
  
- 
+  // Create a new Svix instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
  
   let evt: WebhookEvent
  
- 
+  // Verify the payload with the headers
   try {
     evt = wh.verify(body, {
       "svix-id": svix_id,
