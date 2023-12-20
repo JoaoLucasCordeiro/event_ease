ef8c7daa1dea3a26e4de9d0183dcdf16572cb431
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2023-12-20 14:31:26 -0300

    feat: populated events function added

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -5,6 +5,14 @@ import { handleError } from "../utils"
 import { connectToDatabase } from "../database"
 import User from "../database/models/user.model"
 import Event from "../database/models/event.model"
+import Category from "../database/models/category.model"
+
+
+const populateEvent = (query: any) => {
+    return query
+        .populate({ path: 'organizer', model: User, select: '_id firstName lastName' })
+        .populate({ path: 'category', model: Category, select: '_id name' })
+}
 
 export const createEvent = async ({ event, userId, path }: CreateEventParams) => {
     try {
@@ -25,5 +33,21 @@ export const createEvent = async ({ event, userId, path }: CreateEventParams) =>
         return JSON.parse(JSON.stringify(newEvent));
     } catch (error) {
         handleError(error);
+    }
+}
+
+export const getEventById = async (eventId: String) => {
+    try {
+        await connectToDatabase();
+
+        const event = await populateEvent(Event.findById(eventId));
+
+        if (!event) {
+            throw new Error("Evento não encontrado");
+        }
+
+        return JSON.parse(JSON.stringify(event));
+    } catch (error) {
+        handleError(error)
     }
 }
