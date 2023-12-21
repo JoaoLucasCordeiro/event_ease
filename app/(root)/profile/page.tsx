4f04fa98973e522d9d7749887a622058207dbf9e
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2023-12-21 12:32:54 -0300

    feat: profile page added

---
+++ app/(root)/profile/page.tsx
@@ -0,0 +1,68 @@
+import Collection from '@/components/shared/Collection'
+import { Button } from '@/components/ui/button'
+import { getEventsByUser } from '@/lib/actions/event.actions'
+import { auth } from '@clerk/nextjs'
+import Link from 'next/link'
+import React from 'react'
+
+const ProfilePage = async () => {
+
+    const { sessionClaims } = auth()
+    const userId = sessionClaims?.userId as string;
+
+    const organizedEvents = await getEventsByUser({userId, page: 1})
+
+    return (
+        <>
+            <section className='bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10'>
+                <div className='wrapper flex items-center justify-center sm:justify-between'>
+                    <h3 className='h3-bold text-center sm:text-left'>Minhas compras</h3>
+                    <Button asChild className='button hidden sm:flex' size="lg">
+                        <Link href="/#events">
+                            Explorar mais eventos
+                        </Link>
+                    </Button>
+                </div>
+            </section>
+
+            {/* <section className='wrapper my-8'>
+                <Collection
+                    data={events?.data}
+                    emptyTitle="Você não comprou ingressos de nenhum evento"
+                    emptyStateSubtext="Explore por mais eventos"
+                    collectionType="All_Events"
+                    limit={3}
+                    page={1}
+                    urlParamName="odersPage"
+                    totalPages={2}
+                />
+            </section> */}
+
+            <section className='bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10'>
+                <div className='wrapper flex items-center justify-center sm:justify-between'>
+                    <h3 className='h3-bold text-center sm:text-left'>Eventos organizados</h3>
+                    <Button asChild className='button hidden sm:flex' size="lg">
+                        <Link href="/events/create">
+                            Criar novo evento
+                        </Link>
+                    </Button>
+                </div>
+            </section>
+
+            <section className='wrapper my-8'>
+                <Collection
+                    data={organizedEvents?.data}
+                    emptyTitle="Você não criou nenhum evento"
+                    emptyStateSubtext="Aproveite e crie agora mesmo"
+                    collectionType="Events_Organized"
+                    limit={3}
+                    page={1}
+                    urlParamName="eventsPage"
+                    totalPages={2}
+                />
+            </section>
+        </>
+    )
+}
+
+export default ProfilePage
