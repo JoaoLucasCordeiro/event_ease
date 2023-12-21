188c5e7e43be16aec32f272d0f15351748af0611
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2023-12-21 15:30:47 -0300

    fix: fix the profile page

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,16 +1,23 @@
 import Collection from '@/components/shared/Collection'
 import { Button } from '@/components/ui/button'
 import { getEventsByUser } from '@/lib/actions/event.actions'
+import { getOrdersByUser } from '@/lib/actions/order.actions'
+import { IOrder } from '@/lib/database/models/order.model'
+import { SearchParamProps } from '@/types'
 import { auth } from '@clerk/nextjs'
 import Link from 'next/link'
 import React from 'react'
 
 const ProfilePage = async () => {
 
-    const { sessionClaims } = auth()
+    const { sessionClaims } = auth();
     const userId = sessionClaims?.userId as string;
-
-    const organizedEvents = await getEventsByUser({userId, page: 1})
+  
+  
+    const orders = await getOrdersByUser({ userId, page: 1})
+  
+    const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
+    const organizedEvents = await getEventsByUser({ userId, page: 1 })
 
     return (
         <>
@@ -25,10 +32,10 @@ const ProfilePage = async () => {
                 </div>
             </section>
 
-            {/* <section className='wrapper my-8'>
+            <section className='wrapper my-8'>
                 <Collection
-                    data={events?.data}
-                    emptyTitle="Você não comprou ingressos de nenhum evento"
+                    data={orderedEvents}
+                    emptyTitle="Você não comprou entradas para nenhum evento"
                     emptyStateSubtext="Explore por mais eventos"
                     collectionType="All_Events"
                     limit={3}
@@ -36,7 +43,7 @@ const ProfilePage = async () => {
                     urlParamName="odersPage"
                     totalPages={2}
                 />
-            </section> */}
+            </section>
 
             <section className='bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10'>
                 <div className='wrapper flex items-center justify-center sm:justify-between'>
