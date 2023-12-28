9c95622c47eae56d3fe659b6f65d87ce42f52659
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2023-12-28 11:45:14 -0300

    feat: Added location in the Clerk - pt-BR

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs';
+import { ptBR } from "@clerk/localizations";
 import './globals.css'
 
 const poppins = Poppins({
@@ -23,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider localization={ptBR}>
       <html lang="en">
         <body className={poppins.variable}>{children}</body>
       </html>
